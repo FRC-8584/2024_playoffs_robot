178fe8111ab8f26cd01f6f9c5011dac6ca69c79f
yellowcat9487 <yellowcatwhat@gmail.com>
2024-08-04 11:08:51 +0800

    rename JoystickShooter cmd

--- src/main/java/frc/robot/commands/JoystickShooter.java
+++ src/main/java/frc/robot/commands/JoystickShooter.java
@@ -3,12 +3,12 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Shooter;
 
-public class JoystickShoter extends Command {
+public class JoystickShooter extends Command {
   private Shooter m_shooter;
 
   private double m_power;
   
-  public JoystickShoter(Shooter shooter, double power) {
+  public JoystickShooter(Shooter shooter, double power) {
     m_shooter = shooter;
     m_power = power;
     addRequirements(m_shooter);
