4744ad3ba459a0d1772783b75f85dc833ae7606a
Neoxy <neox9487@gmail.com>
2024-08-08 13:34:34 +0800

    add climber

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,7 +1,34 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase {
-  
+  private CANSparkMax Lmotor = new CANSparkMax(16, MotorType.kBrushed);
+  private CANSparkMax Rmotor = new CANSparkMax(17, MotorType.kBrushed);
+    private CANSparkMax Backmotor = new CANSparkMax(15, MotorType.kBrushed);
+
+  public Climber() {
+  }
+
+  public void up() {
+    Backmotor.set(0.1);
+    Lmotor.set(1);
+    Rmotor.set(1);
+  }
+
+  public void down() {
+    Backmotor.set(-0.1);
+    Lmotor.set(1);
+    Rmotor.set(1);
+  }
+
+  public void stop() {
+    Backmotor.set(0);
+    Lmotor.set(0);
+    Rmotor.set(0);
+  }
+
 }
